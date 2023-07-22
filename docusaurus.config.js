7e1c44e1098a2ce92097369c275a5a69ae23324a
Space V <ahcene.zidahnal@epitech.eu>
2023-07-22 14:26:49 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'idev-co', // Usually your GitHub org/user name.
-  projectName: 'docs.idevco.github.io', // Usually your repo name.
+  projectName: 'docs', // Usually your repo name.
   deploymentBranch: 'gh-pages',
   trailingSlash: false,
 
